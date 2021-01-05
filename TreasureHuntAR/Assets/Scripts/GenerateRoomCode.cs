70ef285baafa88096e098097b7ed854b3b044001
Robert Frentz <frentzrobert@yahoo.com>
2021-01-05 20:32:12 +0200

    Creating and joining rooms using room code

---
+++ TreasureHuntAR/Assets/Scripts/GenerateRoomCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GenerateRoomCode : MonoBehaviour
+{
+    Text roomCode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomCode = GetComponent<Text>();
+        roomCode.text = "Room code : " + RandomString(4);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string RandomString(int length)
+    {
+        System.Random random = new System.Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
--- TreasureHuntAR/Assets/Scripts/PhotonConnectionMaster.cs
+++ TreasureHuntAR/Assets/Scripts/PhotonConnectionMaster.cs
@@ -1,16 +1,16 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PhotonConnection : MonoBehaviourPunCallbacks
+public class PhotonConnectionMaster : MonoBehaviourPunCallbacks
 {
     #region Private Serializable Fields
     [SerializeField]
     private byte maxPlayersPerRoom = 4;
+    private string roomCode = "0000";
 
     #endregion
 
@@ -66,7 +66,7 @@ public class PhotonConnection : MonoBehaviourPunCallbacks
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.IsConnected)
         {
-
+            
         }
         else
         {
@@ -76,47 +76,22 @@ public class PhotonConnection : MonoBehaviourPunCallbacks
         }
     }
 
-
     public void CreateRoom()
     {
-            Text roomCode = GameObject.Find("RoomCode").GetComponent<Text>();
-            Debug.Log("Created room with code: " + roomCode.text);
-            PhotonNetwork.CreateRoom(roomCode.text, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        Text roomCode = GameObject.Find("RoomCode").GetComponent<Text>();
+        Debug.Log("Created room with code: " + roomCode);
+        PhotonNetwork.CreateRoom(roomCode.text, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
     }
 
-    public void JoinRoom()
-    {
-            InputField roomCode = GameObject.Find("InputField").GetComponent<InputField>();
-            Debug.Log("Joining room with code: " + roomCode.text);
-            PhotonNetwork.JoinRoom(roomCode.text);
-    }
     #region MonoBehaviourPunCallbacks Callbacks
-
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
-    }
-
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
     }
 
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available");
 
-        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-        //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-    }
-
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
-        #endregion
-    }
+    #endregion
 
     #endregion
 }
