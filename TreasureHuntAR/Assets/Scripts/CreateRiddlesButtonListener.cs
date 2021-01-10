b264f53edf344d0a3dbfa6f63b67799ff396506d
drgvszi <33577111+drgvszi@users.noreply.github.com>
2021-01-10 12:24:30 +0200

    lil color in your life

---
+++ TreasureHuntAR/Assets/Scripts/CreateRiddlesButtonListener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CreateRiddlesButtonListener : MonoBehaviour
+{
+    GameObject riddle;
+    Button myButton;
+    void Start()
+    {
+       /* myButton = GetComponent<Button>();
+        GameObject[] riddles = GameObject.FindGameObjectsWithTag("Gege");
+
+        myButton.onClick.AddListener(() => { riddle.GetComponent<GetRiddles>().StoreRiddles(); });*/
+
+    }
+}
