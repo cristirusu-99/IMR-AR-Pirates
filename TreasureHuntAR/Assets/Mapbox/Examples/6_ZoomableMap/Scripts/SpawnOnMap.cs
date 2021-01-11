cf1f3cdace643059880dfc86686c04e19a3d47b7
Robert Frentz <frentzrobert@yahoo.com>
2021-01-11 16:12:31 +0200

    Fixed Touch on Map when choosing riddles and treasure location

--- TreasureHuntAR/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ TreasureHuntAR/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -25,6 +25,9 @@
         GameObject[] markers;
         GameObject[] _spawnedObjects;
         public Camera _referenceCamera;
+        public Vector2 startPos;
+        public Vector2 currentPos;
+        public Vector2 endPos;
 
         void Start()
         {
@@ -33,28 +36,78 @@
             GameObject instance;
             for (int i = 0, j = 0; j < ScenesData.numberOfRiddles; i += 2, j++)
             {
-                if(ScenesData.riddlesCoords[i] != 0)
+                if (ScenesData.riddlesCoords[i] != 0)
                 {
                     instance = Instantiate(markers[j]);
                     _locations[j] = new Vector2d(ScenesData.riddlesCoords[i], ScenesData.riddlesCoords[i + 1]);
                     _spawnedObjects[j] = instance;
-                    
+
                 }
             }
 
             if (ScenesData.treasureCoords[0] != 0)
             {
-                 instance = Instantiate(markers[5]);
+                instance = Instantiate(markers[5]);
                 _locations[5] = new Vector2d(ScenesData.treasureCoords[0], ScenesData.treasureCoords[1]);
                 _spawnedObjects[5] = instance;
-                
+
             }
         }
 
-        private void Update()
+        void Update()
         {
-            
-            if (Input.GetMouseButtonUp(1))
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        startPos = touch.position;
+                        break;
+                    case TouchPhase.Moved:
+                        break;
+
+                    case TouchPhase.Ended:
+                        Vector2 touchPos = touch.position;
+                        if (touchPos == startPos)
+                        {
+                            startPos = new Vector2(0, 0);
+                            //assign distance of camera to ground plane to z, otherwise ScreenToWorldPoint() will always return the position of the camera
+                            //http://answers.unity3d.com/answers/599100/view.html
+                            var pos = _referenceCamera.ScreenToWorldPoint(new Vector3(touchPos.x, touchPos.y, _referenceCamera.transform.localPosition.y));
+                            var latlongDelta = _map.WorldToGeoPosition(pos);
+                            GameObject instance;
+                            if (ScenesData.treasForT == true)
+                            {
+                                _locations[5] = latlongDelta;
+                                ScenesData.AddNewTreasureCord(latlongDelta);
+                                if (_spawnedObjects[5] == null)
+                                {
+                                    instance = Instantiate(markers[5]);
+                                    _spawnedObjects[5] = instance;
+                                }
+                            }
+                            else
+                            {
+                                _locations[ScenesData.currentRiddle - 1] = latlongDelta;
+                                ScenesData.AddNewRiddleCoords(latlongDelta);
+                                if (_spawnedObjects[ScenesData.currentRiddle - 1] == null)
+                                {
+                                    instance = Instantiate(markers[ScenesData.currentRiddle - 1]);
+                                    _spawnedObjects[ScenesData.currentRiddle - 1] = instance;
+                                }
+                            }
+                            if (ScenesData.treasForT == true)
+                                SceneManager.LoadScene("Treasure");
+                            else
+                                SceneManager.LoadScene("Riddle");
+                        }
+                        break;
+                }
+
+            }
+            else if (Input.GetMouseButtonUp(1))
             {
                 var mousePosScreen = Input.mousePosition;
                 //assign distance of camera to ground plane to z, otherwise ScreenToWorldPoint() will always return the position of the camera
@@ -67,40 +120,39 @@
                 {
                     _locations[5] = latlongDelta;
                     ScenesData.AddNewTreasureCord(latlongDelta);
-                    if(_spawnedObjects[5] == null)
+                    if (_spawnedObjects[5] == null)
                     {
                         instance = Instantiate(markers[5]);
                         _spawnedObjects[5] = instance;
                     }
-                    
-                   
-
                 }
                 else
                 {
                     _locations[ScenesData.currentRiddle - 1] = latlongDelta;
                     ScenesData.AddNewRiddleCoords(latlongDelta);
-                    if(_spawnedObjects[ScenesData.currentRiddle - 1] == null)
+                    if (_spawnedObjects[ScenesData.currentRiddle - 1] == null)
                     {
                         instance = Instantiate(markers[ScenesData.currentRiddle - 1]);
                         _spawnedObjects[ScenesData.currentRiddle - 1] = instance;
-                    }  
+                    }
                 }
                 if (ScenesData.treasForT == true)
                     SceneManager.LoadScene("Treasure");
                 else
                     SceneManager.LoadScene("Riddle");
+
+
             }
             int count = _spawnedObjects.Length;
             for (int i = 0; i < count; i++)
             {
-                if(_spawnedObjects[i] != null)
+                if (_spawnedObjects[i] != null)
                 {
                     var spawnedObject = _spawnedObjects[i];
                     var location = _locations[i];
                     spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
                     spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-                }  
+                }
             }
 
         }
